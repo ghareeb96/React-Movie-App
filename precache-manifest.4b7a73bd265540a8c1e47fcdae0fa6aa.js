b666185fc5c4d117a3ca657c7d3f0b5d1e17c326
unknown <ghareeb_shehata@outlook.com>
2020-10-22 01:05:44 +0200

    Updates

---
+++ precache-manifest.4b7a73bd265540a8c1e47fcdae0fa6aa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d892097ab992a0204ba0943489503ee9",
+    "url": "/React-Movie-App/index.html"
+  },
+  {
+    "revision": "11e6bb975904757640ca",
+    "url": "/React-Movie-App/static/css/main.8600cf1d.chunk.css"
+  },
+  {
+    "revision": "1688be9fdff82760e60b",
+    "url": "/React-Movie-App/static/js/2.d280f4ee.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React-Movie-App/static/js/2.d280f4ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11e6bb975904757640ca",
+    "url": "/React-Movie-App/static/js/main.ab6a89e0.chunk.js"
+  },
+  {
+    "revision": "c52b32abc7be87a69aca",
+    "url": "/React-Movie-App/static/js/runtime-main.5e18f0ac.js"
+  },
+  {
+    "revision": "aa2aa993e1b3003be2700c8e0918d881",
+    "url": "/React-Movie-App/static/media/0.aa2aa993.jpg"
+  },
+  {
+    "revision": "0af8103ee8b8fd291fb74c612596f616",
+    "url": "/React-Movie-App/static/media/1.0af8103e.jpg"
+  },
+  {
+    "revision": "578047fd0b533b5e73da4a2a78754c72",
+    "url": "/React-Movie-App/static/media/2.578047fd.jpg"
+  },
+  {
+    "revision": "f12902024f7b77ee4c34557ee6f3818b",
+    "url": "/React-Movie-App/static/media/impact.f1290202.ttf"
+  }
+]);
