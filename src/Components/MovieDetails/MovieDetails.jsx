677d9a716021e675b3e9ecc220d798896fbecc41
unknown <ghareeb_shehata@outlook.com>
2020-11-04 16:34:17 +0200

    recommendations and Similar added to movie details page and some errors handled

--- src/Components/MovieDetails/MovieDetails.jsx
+++ src/Components/MovieDetails/MovieDetails.jsx
@@ -9,6 +9,10 @@ const MovieDetails = ({ match }) => {
 
     const [movie, getMovie] = useState({});
     const [credits, setCredits] = useState([]);
+    const [similar, setSimilar] = useState([]);
+    const [recommends, setRecommends] = useState([]);
+
+
     const fetchData = () => {
         fetch(`https://api.themoviedb.org/3/movie/${match.params.id}?api_key=${api_key}`)
             .then(res => res.json())
@@ -20,17 +24,33 @@ const MovieDetails = ({ match }) => {
             .then(res => res.json())
             .then(data => setCredits(data.cast))
     }
-    console.log(credits);
+
+    const getSimilar = () => {
+        fetch(`https://api.themoviedb.org/3/movie/${match.params.id}/similar?api_key=${api_key}`)
+            .then(res => res.json())
+            .then(data => setSimilar(data.results))
+    }
+
+    const getRecommends = () => {
+        fetch(`https://api.themoviedb.org/3/movie/${match.params.id}/recommendations?api_key=${api_key}`)
+            .then(res => res.json())
+            .then(data => setRecommends(data.results))
+    }
 
     useEffect(() => {
         fetchData();
         getCredits();
+        getSimilar();
+        getRecommends();
     }, [])
 
     if (movie.genres) {
         return (
             <div className="movie-details">
                 <div className="details">
+                    <div className="bg-img">
+                        <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} />
+                    </div>
                     <div className="left-section">
                         <div className="poster">
                             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
@@ -42,52 +62,100 @@ const MovieDetails = ({ match }) => {
                     </div>
 
                     <div className="right-section">
-                        <div className="bg-img">
-                            <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} />
-                        </div>
-                        <div className="title">
-                            <h1>
-                                {`${movie.title}  (${movie.release_date.slice(0, 4)})`}
-                            </h1>
-                        </div>
-                        <div className="tagline">
-                            <h2>"{movie.tagline}"</h2>"
-                        </div>
-                        <div className="genres">
-                            <ul>
-                                {movie.genres.map(item => {
-                                    return (<li key={item.id} >{item.name}</li>)
-                                })}
-                            </ul>
-                        </div>
-                        <div className="overview">
-                            <div className="left">
-                                <h2>Overview</h2>
-                            </div>
-                            <div className="right">
-                                <p>{movie.overview}</p>
-                            </div>
-                        </div>
 
-                        <div className="cast">
-                            <div className="left">
-                                <h2>Cast</h2>
+                        <div className="right-details">
+                            <div className="title">
+                                <h1>
+                                    {`${movie.title}  (${movie.release_date.slice(0, 4)})`}
+                                </h1>
                             </div>
-                            <div className="right">
-                                {credits
-                                    .filter(item => item.profile_path !== null)
-                                    .filter((item, index) => index < 8)
-                                    .map((character) => {
-                                        return (
-                                            <div key={character.id} className="profile-container">
-                                                <img src={`https://image.tmdb.org/t/p/w500${character.profile_path}`}
-                                                    alt="Profile" />
-                                            </div>
-                                        )
+
+                            {movie.tagline !== "" ?
+
+                                <div className="tagline">
+                                    <h2>"{movie.tagline}"</h2>"
+                                    </div>
+
+                                :
+                                ""
+                            }
+
+
+                            <div className="genres">
+                                <ul>
+                                    {movie.genres.map(item => {
+                                        return (<li key={item.id} >{item.name}</li>)
                                     })}
+                                </ul>
                             </div>
-                        </div>
+                            <div className="overview">
+                                <div className="left">
+                                    <h2>Overview</h2>
+                                </div>
+                                <div className="right">
+                                    <p>{movie.overview}</p>
+                                </div>
+                            </div>
 
+                            <div className="cast">
+                                <div className="left">
+                                    <h2>Cast</h2>
+                                </div>
+                                <div className="right">
+                                    {credits
+                                        .filter((item, index) => item.profile_path !== null && index < 8)
+                                        .map((character) => {
+                                            return (
+                                                <div key={character.id} className="profile-container">
+                                                    <img src={`https://image.tmdb.org/t/p/w500${character.profile_path}`}
+                                                        alt="Profile" />
+                                                </div>
+                                            )
+                                        })}
+                                </div>
+                            </div>
+                            {
+                                similar.length === 0 ? "" :
+                                    <div className="similar">
+                                        <div className="top">
+                                            <h2>Similar</h2>
+                                        </div>
+                                        <div className="body">
+                                            {similar
+                                                .filter((item, index) => item.poster_path !== null && index < 10)
+                                                .map((item) => {
+                                                    return (
+                                                        <div key={item.id} className="container">
+                                                            <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                                                                alt="Poster" />
+                                                        </div>
+                                                    )
+                                                })}
+                                        </div>
+                                    </div>
+                            }
+
+                            {
+                                recommends.length === 0 ? "" :
+                                    <div className="recommends">
+                                        <div className="top">
+                                            <h2>Recommendations</h2>
+                                        </div>
+                                        <div className="body">
+                                            {recommends
+                                                .filter((item, index) => item.poster_path !== null && index < 10)
+                                                .map((item) => {
+                                                    return (
+                                                        <div key={item.id} className="container">
+                                                            <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                                                                alt="Poster" />
+                                                        </div>
+                                                    )
+                                                })}
+                                        </div>
+                                    </div>
+                            }
+                        </div>
                     </div>
 
 
