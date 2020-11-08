d2c71373c2daf593c684dc00472828401e69fec5
unknown <ghareeb_shehata@outlook.com>
2020-11-08 03:18:42 +0200

    Watchlist & Watchedlist Pages are Done with their styles

--- src/Components/WatchListPage/WatchList.jsx
+++ src/Components/WatchListPage/WatchList.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Watchlist.scss";
+import MoviesContainer from "../MoviesContainer/MoviesContainer"
 
+const api_key = "137436a3a883e2b94597a24e32d9d6b8";
 
 const WatchList = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [tvShows, setTvShows] = useState([]);
+
+
+    useEffect(() => {
+        const storedMovies = JSON.parse(localStorage.getItem("watchlistMovies"));
+        storedMovies.map(item => {
+            fetch(`https://api.themoviedb.org/3/movie/${item}?api_key=${api_key}`)
+                .then(res => res.json())
+                .then(data => setMovies(old => old.concat(data)))
+        })
+
+
+        const storedTvShows = JSON.parse(localStorage.getItem("watchlistTV"));
+        storedTvShows.map(item => {
+            fetch(`https://api.themoviedb.org/3/tv/${item}?api_key=${api_key}`)
+                .then(res => res.json())
+                .then(data => setTvShows(old => old.concat(data)))
+        })
+
+    }, [])
+
+
     return (
         <div className="watch-list" >
-            <div className="slogan">
-                <h1>Watchlist</h1>
+            <div className="container" >
+                <div className="legend">
+                    <h3>Movies Watchlist</h3>
+                </div>
+                <div className="items-container">
+                    <MoviesContainer
+                        movies={movies}
+                        type="movie" />
+                </div>
+            </div>
+            <div className="container" >
+                <div className="legend">
+                    <h3>TV Shows Watchlist</h3>
+                </div>
+                <div className="items-container">
+                    <MoviesContainer
+                        movies={tvShows}
+                        type="tv" />
+                </div>
             </div>
         </div>
     )
--- src/Components/WatchedListPage/WatchedList.jsx
+++ src/Components/WatchedListPage/WatchedList.jsx
@@ -1,15 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./WatchedList.scss";
+import MoviesContainer from "../MoviesContainer/MoviesContainer"
 
 
+const api_key = "137436a3a883e2b94597a24e32d9d6b8";
 
 const WatchedList = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [tvShows, setTvShows] = useState([]);
+
+    useEffect(() => {
+        const storedMovies = JSON.parse(localStorage.getItem("watchedMovies"));
+        storedMovies.map(item => {
+            fetch(`https://api.themoviedb.org/3/movie/${item}?api_key=${api_key}`)
+                .then(res => res.json())
+                .then(data => setMovies(old => old.concat(data)))
+        })
+
+
+        const storedTvShows = JSON.parse(localStorage.getItem("watchedTV"));
+        storedTvShows.map(item => {
+            fetch(`https://api.themoviedb.org/3/tv/${item}?api_key=${api_key}`)
+                .then(res => res.json())
+                .then(data => setTvShows(old => old.concat(data)))
+        })
+
+    }, [])
 
 
     return (
         <div className="watched-list"  >
-            <div className="slogan">
-                <h1>Watched List</h1></div>
+            <div className="container" >
+                <div className="legend">
+                    <h3>Watched Movies</h3>
+                </div>
+                <div className="items-container">
+                    <MoviesContainer
+                        movies={movies}
+                        type="movie" />
+                </div>
+            </div>
+            <div className="container" >
+                <div className="legend">
+                    <h3>Watched TV Shows</h3>
+                </div>
+                <div className="items-container">
+                    <MoviesContainer
+                        movies={tvShows}
+                        type="tv" />
+                </div>
+            </div>
         </div>
     )
 }
