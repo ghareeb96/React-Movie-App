86cc15fddb82981cbfe60834f59d220f1fc1b1ff
unknown <ghareeb_shehata@outlook.com>
2020-11-12 10:53:08 +0200

    Updates

--- precache-manifest.87bd1a3521d960caedf85de2919baffa.js
+++ precache-manifest.87bd1a3521d960caedf85de2919baffa.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "55dc334fb5e325822a46e1d84b2f5ad9",
+    "revision": "3d35d934980ba351dffbc58a806f1e9f",
     "url": "/React-Movie-App/index.html"
   },
   {
-    "revision": "629a0c82ac5ee2245ff4",
-    "url": "/React-Movie-App/static/css/main.8db06bce.chunk.css"
+    "revision": "eb00a9ebc5284ecdf176",
+    "url": "/React-Movie-App/static/css/main.7a3bb5a2.chunk.css"
   },
   {
-    "revision": "3f6923d7fc50b1c77718",
-    "url": "/React-Movie-App/static/js/2.4a995561.chunk.js"
+    "revision": "d238129ac89f99b389fb",
+    "url": "/React-Movie-App/static/js/2.07caf240.chunk.js"
   },
   {
     "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
-    "url": "/React-Movie-App/static/js/2.4a995561.chunk.js.LICENSE"
+    "url": "/React-Movie-App/static/js/2.07caf240.chunk.js.LICENSE"
   },
   {
-    "revision": "629a0c82ac5ee2245ff4",
-    "url": "/React-Movie-App/static/js/main.9842a061.chunk.js"
+    "revision": "eb00a9ebc5284ecdf176",
+    "url": "/React-Movie-App/static/js/main.36422a9d.chunk.js"
   },
   {
     "revision": "c52b32abc7be87a69aca",
