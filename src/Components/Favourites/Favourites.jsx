1386c59cfd251df08df8362e195fb21e1708d92b
Ghareeb Shehata <ghreeb_shehata@outlook.com>
2022-06-16 02:15:11 +0200

    final modifications

--- src/Components/Favourites/Favourites.jsx
+++ src/Components/Favourites/Favourites.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import "./Favourites.scss";
 import CardsContainer from '../CardsContainer/CardsContainer';
 
 const Favourites = ({ api_key }) => {
